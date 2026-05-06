3c7882b70da9539fd430ed87081b4c10d1b622e5
gravo0801 <fnaticdoc@gmail.com>
2026-05-06 19:54:36 +0900

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TravelStudio from './TravelStudio.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TravelStudio />
-  </React.StrictMode>,
-)
